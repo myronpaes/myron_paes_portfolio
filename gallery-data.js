bbf843905e7fbcdfbcb623bc9d3e3b20d17b6724
Myron Paes <myronpaes@gmail.com>
2026-03-24 21:15:25 -0400

    Add gallery data and enhance image loading for improved performance

---
+++ gallery-data.js
@@ -0,0 +1,34 @@
+const galleryData = [
+  {
+    file: "pic1.webp",
+    text: "Winning 3rd prize with Tapyoca at UPitch NJ 2025",
+  },
+  {
+    file: "pic2.webp",
+    text: "Presenting at NJIT's Spring 2024 Honors Interdisciplinary Research Forum",
+  },
+  {
+    file: "pic3.webp",
+    text: "Exhibiting Sensory Stress at Cherokee High School's Project Lead the Way CAPSTONE conference",
+  },
+  {
+    file: "pic4.webp",
+    text: "My 2024 research on Standardized Testing Results and Primary School Expenditure",
+  },
+  { 
+    file: "pic5.webp", 
+    text: "Speaking at my Eagle Scout Ceremony" 
+  },
+  {
+    file: "pic6.webp",
+    text: "Moving into NJIT with my brother Keith (pictured right)",
+  },
+  {
+    file: "pic7.webp",
+    text: "Displaying menstrual care startup Crimson Care at NJIT's Fall 2024 Honors Interdisciplinary Research Forum",
+  },
+  {
+    file: "pic8.webp",
+    text: 'Won 1st prize at Rowan University\'s ProfHacks 2026 for Frontier Scout, an asteroid "bounty hunter" project',
+  },
+];
